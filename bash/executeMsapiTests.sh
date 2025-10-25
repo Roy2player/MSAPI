3f92cbbee8b44fb334eac5286e36184be30f34a2
Roy2player <72526152+Roy2player@users.noreply.github.com>
2025-10-25 18:24:06 +0300

    Server cannot start if was stopped before

--- bash/executeMsapiTests.sh
+++ bash/executeMsapiTests.sh
@@ -16,7 +16,7 @@ fi
 bash $(dirname ${BASH_SOURCE})/buildMsapiLib.sh
 ExitIfError $?
 
-declare -a tests=("units" "applicationHandlers" "objectProtocol" "http")
+declare -a tests=("units" "applicationHandlers" "objectProtocol" "http" "server")
 
 for i in "${tests[@]}"; do
     RunCommand "cmake -DCMAKE_BUILD_TYPE=${BUILD_PROFILE} -B ${MSAPI_PATH}/tests/${i}/build ${MSAPI_PATH}/tests/${i}/build \
